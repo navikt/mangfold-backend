b7fe18adb69ac7cebe237293bc73c02f9d7b4bc0
Sarah <sarah.refsdal@nav.no>
2025-07-02 10:55:56 +0200

    legg til CORS for kobling med frontend

--- src/main/kotlin/no/nav/Main.kt
+++ src/main/kotlin/no/nav/Main.kt
@@ -6,6 +6,8 @@ import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.json.Json
 import no.nav.routes.KjonnRoutes
+import io.ktor.server.plugins.cors.routing.*
+import io.ktor.http.*
 
 fun main(args: Array<String>): Unit = EngineMain.main(args)
 
@@ -14,6 +16,12 @@ fun Application.module() {
     val projectId = config.propertyOrNull("bigquery.projectId")?.getString()
         ?: System.getenv("BIGQUERY_PROJECT_ID")
         ?: throw IllegalStateException("BIGQUERY_PROJECT_ID må settes")
+    
+    install(CORS) {
+        anyHost() // Mulig denne kan/må endres
+        allowHeader(HttpHeaders.ContentType)
+        allowMethod(HttpMethod.Get)
+    }
 
     install(ContentNegotiation) {
         json(Json {
